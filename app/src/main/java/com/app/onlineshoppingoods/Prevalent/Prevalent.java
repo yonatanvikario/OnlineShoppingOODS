274c95d6e714986ef5fffcde3debf5adbc2ded05
BeguLadang <yonatanparapat@gmail.com>
2019-07-11 20:51:11 +0700

    Commit

---
+++ app/src/main/java/com/app/onlineshoppingoods/Prevalent/Prevalent.java
@@ -0,0 +1,9 @@
+package com.app.onlineshoppingoods.Prevalent;
+import com.app.onlineshoppingoods.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
