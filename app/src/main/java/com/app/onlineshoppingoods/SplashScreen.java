c0f89e00ca97187af3aaa29a3ce37d2407d2aafe
BeguLadang <yonatanparapat@gmail.com>
2019-07-16 12:56:48 +0700

    Commit

---
+++ app/src/main/java/com/app/onlineshoppingoods/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.app.onlineshoppingoods;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+    private int waktu_loading=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(SplashScreen.this, LoginActivity.class); //sebelumnya Login
+                startActivity(home);
+                finish();
+
+            }
+        },waktu_loading);
+    }
+}
